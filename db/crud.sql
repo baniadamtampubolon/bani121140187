f4f565163438131ffb954a2faa02ffa16273fe4f
baniiiiiiiiii <118009209+baniadamtampubolon@users.noreply.github.com>
2023-12-18 10:39:54 +0700

    Update crud.sql

--- db/crud.sql
+++ db/crud.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 19 Jun 2022 pada 16.07
--- Versi Server: 10.1.19-MariaDB
--- PHP Version: 5.6.28
+-- Generation Time: Dec 18, 2023 at 03:58 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,23 +24,23 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `peserta`
+-- Table structure for table `peserta`
 --
 
-CREATE TABLE peserta (
-  id_peserta int(11) NOT NULL,
-  nama varchar(50) NOT NULL,
-  sekolah varchar(50) NOT NULL,
-  jurusan varchar(50) NOT NULL,
-  no_hp char(13) NOT NULL,
-  alamat varchar(50) NOT NULL
-);
+CREATE TABLE `peserta` (
+  `id_peserta` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `sekolah` varchar(50) NOT NULL,
+  `jurusan` varchar(50) NOT NULL,
+  `no_hp` char(13) NOT NULL,
+  `alamat` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data untuk tabel `peserta`
+-- Dumping data for table `peserta`
 --
 
-INSERT INTO peserta (id_peserta, nama, sekolah, jurusan, no_hp, alamat) VALUES
+INSERT INTO `peserta` (`id_peserta`, `nama`, `sekolah`, `jurusan`, `no_hp`, `alamat`) VALUES
 (1, 'John Doe', 'SMA 1 Jakarta', 'IPA', '081234567890', 'Jl. Merdeka No. 123'),
 (2, 'Jane Smith', 'SMA 2 Surabaya', 'IPS', '085678912345', 'Jl. Jendral Sudirman No. 45'),
 (3, 'Michael Johnson', 'SMK 3 Bandung', 'Teknik Mesin', '081112223344', 'Jl. Pahlawan No. 67'),
@@ -49,8 +50,8 @@ INSERT INTO peserta (id_peserta, nama, sekolah, jurusan, no_hp, alamat) VALUES
 (7, 'Daniel Chen', 'SMK 7 Denpasar', 'Teknik Informatika', '085678912345', 'Jl. Diponegoro No. 121'),
 (8, 'Olivia Tan', 'SMK 8 Palembang', 'Kimia', '081112223344', 'Jl. Mangkubumi No. 131'),
 (9, 'Ethan Nguyen', 'SMA 9 Bandar Lampung', 'Matematika', '082334455667', 'Jl. Sudirman No. 143'),
-(10, 'Isabella Wong', 'SMA 10 Pontianak', 'Fisika', '083556677889', 'Jl. Thamrin No. 155');
-
+(10, 'Isabella Wong', 'SMA 10 Pontianak', 'Fisika', '083556677889', 'Jl. Thamrin No. 155'),
+(20, 'Jeje', 'Sekolah Lupa Masuk', 'Jurus Seribu Janji', '0818181818818', 'Jalan Jalan');
 
 --
 -- Indexes for dumped tables
@@ -59,18 +60,15 @@ INSERT INTO peserta (id_peserta, nama, sekolah, jurusan, no_hp, alamat) VALUES
 --
 -- Indexes for table `peserta`
 --
-ALTER TABLE peserta
-  ADD PRIMARY KEY (id_peserta);
+ALTER TABLE `peserta`
+  ADD PRIMARY KEY (`id_peserta`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table peserta
+-- AUTO_INCREMENT for table `peserta`
 --
-ALTER TABLE peserta
-  MODIFY id_peserta int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE `peserta`
+  MODIFY `id_peserta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
